7f64b2407b398771cafc20e23e5ea98dc6e997b2
ShivXShakti <kuldeeplakhansons@gmail.com>
2024-08-06 10:51:08 +0530

    cpp ros tutorials

--- ros_ws/src/cpp/launch/simple.launch.py
+++ ros_ws/src/cpp/launch/simple.launch.py
@@ -1,10 +1,9 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
 def generate_launch_description():
     return LaunchDescription([
-                            Node(
-                                package='cpp',
-                                executable='simple_node',
-                                output='screen'),
-])
+        Node(
+            package='cpp',
+            executable='simple_node',
+            output='screen'),
+    ])
--- ros_ws/src/cpp/src/simple.cpp
+++ ros_ws/src/cpp/src/simple.cpp
@@ -6,8 +6,12 @@ int main(int argc, char * argv[])
 rclcpp::init(argc, argv);
 // Create a ROS2 node named ObiWan
 auto node = rclcpp::Node::make_shared("ObiWan");
+rclcpp::WallRate loop_rate(2);
+while (rclcpp::ok()){
 // Print a message to the terminal
 RCLCPP_INFO(node->get_logger(), "Help me Obi-Wan Kenobi, you're my only hope");
+rclcpp::spin_some(node);
+loop_rate.sleep();}
 // Shutdown the ROS2 communication
 rclcpp::shutdown();
 return 0;
